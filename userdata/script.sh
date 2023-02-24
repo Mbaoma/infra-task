22f6d548748be4e9dd1260e0ed29826fda39aa43
Mbaoma <mmaryraphaella@gmail.com>
2023-02-24 18:55:40 +0100

    edited infra file

--- userdata/script.sh
+++ userdata/script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-sudo apt update -y &&
-sudo apt install -y nginx
-sudo apt install certbot python3-certbot-nginx -y
+sudo yum update -y &&
+sudo yum install -y nginx
+sudo yum install certbot python3-certbot-nginx -y
 
 # Set the file name and directory path
 file_name="index.html"
