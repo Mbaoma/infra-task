0650f8314557a1573f78dda31f9415c9ea406b1c
Mbaoma <mmaryraphaella@gmail.com>
2023-02-26 15:48:03 +0100

    correct setup

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,22 +1,22 @@
 output "vpc_id" {
-  value = aws_vpc.vpc.id
+  value = aws_default_vpc.default.id
 }
 
 output "public_subnets_id" {
-  value = ["${aws_subnet.public_subnet.*.id}"]
+  value = ["${aws_default_subnet.default_az1.*.id}"]
 }
 
 output "private_subnets_id" {
-  value = ["${aws_subnet.private_subnet.*.id}"]
+  value = ["${aws_default_subnet.default_az2.*.id}"]
 }
 
-output "default_sg_id" {
-  value = aws_security_group.InfraTask-sg.id
-}
+#output "default_sg_id" {
+ # value = aws_security_group.InfraTask-sg.id
+#}
 
-output "public_route_table" {
-  value = aws_route_table.public.id
-}
+#output "public_route_table" {
+ # value = aws_route_table.public.id
+#}
 
 output ip_address {
   value = aws_instance.instance.public_ip
